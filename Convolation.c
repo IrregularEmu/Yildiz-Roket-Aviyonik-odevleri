0b200cf4f5dd1e120d775f40f1a85fd079f5a8f2
IrregularEmu <myigit.yilmaz23@gmail.com>
2025-01-12 18:16:31 +0300

    Update Convolation.c
    Kapanmamış { kapatıldı yanlış değişkenler düzeltildi

--- Convolation.c
+++ Convolation.c
@@ -10,6 +10,7 @@ void printMatrix(int matrix[][100], int M, int N) {
         printf("\n");
     }
 }
+
 // Matris yazma fonksiyonu
 void printmatris(int matrix[][100], int M, int N) {
     for (int i = 0; i < M; i++) {
@@ -18,21 +19,23 @@ void printmatris(int matrix[][100], int M, int N) {
         }
         printf("\n");
     }
+}
 
-void konvolusyon(int G[][100], int M, int N, int F[][100], int k, int R[][100]){
-int filtop=0;
+void konvolusyon(int G[][100], int M, int N, int F[][100], int k, int R[][100]) {
+    int filtop = 0;
 
     for (int satir = 0; satir < k; satir++) {
-    for (int sutun = 0; sutun < k; sutun++) {
-    filtop += F[satir][sutun];} }
+        for (int sutun = 0; sutun < k; sutun++) {
+            filtop += F[satir][sutun];
+        }
+    }
 
-if(filtop==0){
-  printf("Filtreleme matrisi elemanları toplamı 0");
-  end(0);
+    if (filtop == 0) {
+        printf("Filtreleme matrisi elemanlari toplami 0\n");
+        exit(0);
+    }
 
-
-}
-  // Konvolüsyon işlemi uygulaması
+    // Konvolüsyon işlemi uygulaması
     for (int satir = 0; satir <= M - k; satir++) {
         for (int sutun = 0; sutun <= N - k; sutun++) {
             int sum = 0;
@@ -46,16 +49,12 @@ if(filtop==0){
             R[satir][sutun] = sum / filtop;
         }
     }
-
-
 }
 
-int main(){
-
+int main() {
     // Degiskenler belirleniyor
     int n;
     int M, N;
-    char direction;
     int matris[100][100];
     int filter[100][100];
     int sonuc[50][50];
@@ -63,15 +62,15 @@ int main(){
     // Matrisin satir ve sutun sayilarini giriyoruz
 inputn:
     printf("Matrisin satir sayisini girin (M): ");
-    if (scanf("%d", &M) != 1 && N<=0) {
-        while ((M = getchar()) != '\n' && M != EOF);
+    if (scanf("%d", &M) != 1 || M <= 0) {
+        while (getchar() != '\n');
         printf("Hata: Lutfen bir tam sayi giriniz.\n");
         goto inputn;
     }
 
     printf("Matrisin sutun sayisini girin (N): ");
-    if (scanf("%d", &N) != 1 && N<=0) {
-        while ((N = getchar()) != '\n' && N != EOF);
+    if (scanf("%d", &N) != 1 || N <= 0) {
+        while (getchar() != '\n');
         printf("Hata: Lutfen bir tam sayi giriniz.\n");
         goto inputn;
     }
@@ -89,44 +88,36 @@ inputn:
     printf("Ana matris:\n");
     printMatrix(matris, M, N);
 
-
-    inputf:
-    printf("Matrisin boyutunu girin (NxN): ");
-
-
-
-   if (scanf("%d", &n) != 1 ) {
-
-        while ((n = getchar()) != '\n' && n != EOF);
-        printf("Hata: Lutfen bir tam sayý giriniz.\n");
-        goto inputn;
-     } else if ( n > M && n>N) {
+inputf:
+    printf("Filtre boyutunu girin (NxN): ");
+    if (scanf("%d", &n) != 1) {
+        while (getchar() != '\n');
+        printf("Hata: Lutfen bir tam sayi giriniz.\n");
+        goto inputf;
+    } else if (n > M || n > N) {
         printf("Hata: Lutfen en az ana matrisin satir veya sütun sayilarina esit deger giriniz.\n");
         goto inputf;
     } else {
-
         printf("Girdiginiz deger: %d\n", n);
     }
 
-    //matristeki degerleri istiyoruz sonra girilen matrisi gosteriyoruz
-    printf("Matrisin degerlerini girin:\n");
+    // Filtre matrisinin degerlerini istiyoruz
+    printf("Filtre matrisi degerlerini girin:\n");
     for (int satir = 0; satir < n; satir++) {
         for (int sutun = 0; sutun < n; sutun++) {
-            scanf("%d", &filter[i][j]);
+            scanf("%d", &filter[satir][sutun]);
         }
     }
 
-
     printf("Filtre matrisi:\n");
-    printmatris(filter,n, n);
+    printmatris(filter, n, n);
 
-
-     // Konvolüsyon iþlemini gerçekleþtir
+    // Konvolüsyon islemini gerceklestir
     konvolusyon(matris, M, N, filter, n, sonuc);
 
-    // Sonuç matrisini yazdýr
+    // Sonuc matrisini yazdir
     printf("Sonuc matrisi:\n");
     printMatrix(sonuc, M - n + 1, N - n + 1);
 
-
+    return 0;
 }
